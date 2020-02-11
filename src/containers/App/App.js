5075c79c69a346c6ed4c62a1533cee17a65e9296
Jack <Jack@MacBook-Pro-de-Johanna.local>
2020-02-11 17:13:59 -0300

    Added basic structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+{/*
+
+  App
+  |__Header
+  | |__Logo
+  | |__Menu items
+  | |__Add icon
+  | |__Bell icon
+  | |__Settings icon
+  |__Body
+  | |__Home card
+  | |__Small cards
+  | |__Large cards
+  | |__Button
+
+*/}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
